d0f307815623a5c453574dfb6b1c7314fd8700a8
Andy Liang <andyzen619@gmail.com>
2020-01-30 03:08:24 -0500

    add-chart: refactored api charts, add events daily chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import DataComponent from "./Components/DataComponent";
+import DataComponent from "./Components/APIDataCharts/EventsHourlyComponent";
+import PoiComponent from "./Components/APIDataCharts/PoiComponent";
+import EventsDailyComponent from "./Components/APIDataCharts/EventsDailyComponent";
 
 function App() {
   return (
     <div className="App">
       <DataComponent/>
+      <PoiComponent/>
+      <EventsDailyComponent/>
     </div>
   );
 }
