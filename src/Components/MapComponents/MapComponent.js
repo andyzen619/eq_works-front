738e61573564f042fd7c82fa55b9df79bb22d7f9
Andy Liang <andyzen619@gmail.com>
2020-01-31 02:28:51 -0500

    add: days of the week left vertical tabs on map component

--- src/Components/MapComponents/MapComponent.js
+++ src/Components/MapComponents/MapComponent.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { GMap } from "primereact/gmap";
-import {} from "semantic-ui-react";
+import { Grid, Menu, Segment } from "semantic-ui-react";
 
 const MapComponent = () => {
   const [apiPoiData, setApiPoiData] = useState([]);
-  const [defaultCenter, setDefaultCenter] = useState({lat: 43.6532, lng: -79.3832});
+  const [defaultCenter, setDefaultCenter] = useState({
+    lat: 43.6532,
+    lng: -79.3832
+  });
+  const [activeItem, setActiveItem] = useState("Sunday");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
 
   const mapOptions = () => {
-
-      return {
-        center: defaultCenter,
-        zoom: 12
-      };
+    return {
+      center: defaultCenter,
+      zoom: 12
+    };
   };
 
   useEffect(() => {
@@ -28,17 +35,63 @@ const MapComponent = () => {
   }, []);
 
   useEffect(() => {
-    if(apiPoiData.length > 0){
-      setDefaultCenter({lat: apiPoiData[3].lat, lng: apiPoiData[3].lon}); 
+    if (apiPoiData.length > 0) {
+      setDefaultCenter({ lat: apiPoiData[3].lat, lng: apiPoiData[3].lon });
     }
-  }, [apiPoiData])
+  }, [apiPoiData]);
 
   return (
     <div>
-      <GMap
-        options={mapOptions()}
-        style={{ width: "100%", minHeight: "320px" }}
-      />
+      <Grid>
+        <Grid.Column width={4}>
+          <Menu fluid vertical tabular>
+            <Menu.Item
+              name="Sunday"
+              active={activeItem === "Sunday"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="Monday"
+              active={activeItem === "Monday"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="Tuesday"
+              active={activeItem === "Tuesday"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="Wednesday"
+              active={activeItem === "Wednesday"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="Thursday"
+              active={activeItem === "Thursday"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="Friday"
+              active={activeItem === "Friday"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="Saturday"
+              active={activeItem === "Saturday"}
+              onClick={handleItemClick}
+            />
+          </Menu>
+        </Grid.Column>
+
+        <Grid.Column stretched width={12}>
+          <Segment>
+            <GMap
+              options={mapOptions()}
+              style={{ width: "100%", minHeight: "320px" }}
+            />
+          </Segment>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 };
